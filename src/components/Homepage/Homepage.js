69b4f0b7f0e9d220fbb4f02573b9dda88dee4192
Harshit Chopra <chopraharshit2000@gmail.com>
2021-06-09 19:51:41 +0530

    Fixed footer and some UI changes

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './Homepage.css';
 import banner from '../images/banner.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router';
 
 function Homepage() {
+
+    const user = useSelector(selectUser);
+    const history = useHistory();
+
+    const goToHome = () => {
+        history.push('/ask-share');
+    }
+
     return (
         <div className="homepage">
 
@@ -10,9 +22,19 @@ function Homepage() {
 
             <div className="banner">
                 <div className="banner_left">
-                    <p className="homepage_title2">Keep up with the latest information, All in one place.</p>
+                    <p className="homepage_title2">Keep up with all the latest happenings.</p>
                     <p className="homepage_title3">Communicate, Share knowledge, Buy needy things, Sell off non needy things, Provide Help, Get Help, Guide students, Get guided by students, know all the ins and outs of our college and plenty more.</p>
-                    <p className="homepage_title4"><span style={{color: '#3f51b5', fontWeight: '700'}}>LOGIN</span> and become an integral part of the Takshshila's growing Student Community.</p>
+                    {(!user) ? (
+                        <p className="homepage_title4"><span style={{color: '#3f51b5', fontWeight: '700'}}>LOGIN</span> and become an integral part of the Takshshila's growing Student Community.</p>
+                    ) : (
+                        <Button
+                           variant="contained" color="primary"
+                           onClick={goToHome}
+                        >
+                            Go to Home 
+                        </Button>
+                    )}
+                    
                 </div>
                 <div className="banner_right">
                     <img src={banner} alt="" />
