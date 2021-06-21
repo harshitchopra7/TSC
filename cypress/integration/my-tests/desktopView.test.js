532473b6e89c304b009bcb80497d4ed3768dc5f3
harshit <chopraharshit2000@gmail.com>
2021-06-22 01:29:58 +0530

    Desktop cypress testing done with 76.58% code coverage

--- cypress/integration/my-tests/desktopView.test.js
+++ cypress/integration/my-tests/desktopView.test.js
@@ -2,13 +2,13 @@
 
 describe('Tests', () => {
     it('Landing page looks good on desktop', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/')
         cy.contains('Welcome to Takshshila').should('exist')
     })
     
     // it('login button working', () => {
-    //     // cy.viewport(1280, 720)
+    //     // cy.viewport(1440, 900)
     //     // cy.visit('http://localhost:3001/')
     //     cy.get('div.nav_right > button') .contains('LOG IN')
     //     cy.url().should('eq', 'http://localhost:3001/ask-share')
@@ -16,53 +16,53 @@ describe('Tests', () => {
     // })
 
     it('Left menu at ask-share page working', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/ask-share')
         cy.get('div.leftmenu_mid_elements > p').contains('Ask')
     })
 
     it('Left menu ask button working', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/buy-sell')
         cy.get('div.leftmenu_mid_elements > p').contains('Ask').click()
         cy.url().should('eq', 'http://localhost:3001/ask-share')
     })
 
     it('Left menu share button working', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/buy-sell')
         cy.get('div.leftmenu_mid_elements > p').contains('Ask').click()
         cy.url().should('eq', 'http://localhost:3001/ask-share')
     })
 
     it('Ask-share input field working', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/ask-share')
         cy.get('div.feedmid_top_input > input').type("Input field testing")
     })
 
     it('Left menu at buy-sell page working', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/buy-sell')
         cy.get('div.leftmenu_mid_elements > p').contains('Buy')
     })
 
     it('Left menu buy button working', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/ask-share')
         cy.get('div.leftmenu_mid_elements > p').contains('Buy').click()
         cy.url().should('eq', 'http://localhost:3001/buy-sell')
     })
 
     it('Left menu sell button working', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/ask-share')
         cy.get('div.leftmenu_mid_elements > p').contains('Sell').click()
         cy.url().should('eq', 'http://localhost:3001/buy-sell')
     })
 
     it('Sell button and sell form inputs working', () => {
-        cy.viewport(1280, 720)
+        cy.viewport(1440, 900)
         cy.visit('http://localhost:3001/buy-sell')
         cy.get('div#blurr > button').click()
         cy.get('input.itemname_input').type("Macbook Air")
